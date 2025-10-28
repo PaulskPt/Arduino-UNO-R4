d03f49c33af5cbf14accb8912cd53feefcb75d7b
Paulus H.J. Schulinck <PaulskPt@users.noreply.github.com>
2025-10-28 12:05:59 +0000

    Update R4WiFi_RTC_NTP_V5.ino
    Removed old NTP system

--- R4WiFi_RTC_NTP_V5/R4WiFi_RTC_NTP_V5.ino
+++ R4WiFi_RTC_NTP_V5/R4WiFi_RTC_NTP_V5.ino
@@ -1,5 +1,5 @@
 /*
- * R4WiFi_RTC_NTP_V5.ino (privately known as: Arduino_UNO_WiFi_R4__RTC_NTP_V5.ino)
+ * Arduino_UNO_WiFi_R4__RTC_NTP_V5.ino (See forked repo: Arduino-UNO-R4, example: R4WiFi_RTC_NTP_V5)
  * This example shows how to set the RTC (Real Time Clock) on the Portenta C33 
  * to the current date and time retrieved from an NTP server on the Internet (pool.ntp.org).
  * Then the current time from the RTC is printed to the Serial port.
@@ -23,7 +23,7 @@
  * I added a serialPrintf() function created by Microsoft Copilot.
  * See https://forum.arduino.cc/t/uno-r4-wifi-adding-eeprom-procedure-gives-flash-overrun/1396203/5,
  * response from user @david_2018:
- * The UNO R4 WiFi does not have actual EEPROM, it is emulated in flash memory, and is 8K bytes in size. For the moment these functions are disabled.
+ * The UNO R4 WiFi does not have actual EEPROM, it is emulated in flash memory, and is 8K bytes in size.
  * Added functionality to read data from a Qwiic connected sensor Pimoroni Breakout Garden BME280 and display the data on the matrix.
  * Update 2025-10-24:
  * Added board unique ID as BANNER4 (using the 107-Arduino-UniqueId library repo)
@@ -31,7 +31,8 @@
  * Update 2025-10-27. Added dst awarenesss (for Portugal timezone)
  */
 
-#include <RTC.h> // See: "C:\Users\<User>\AppData\Local\Arduino15\packages\arduino\hardware\renesas_uno\1.5.1\libraries\RTC\src\RTC.h"
+#include <RTC.h>
+//#include "C:\Users\PaulS2\AppData\Local\Arduino15\packages\arduino\hardware\renesas_uno\1.5.1\libraries\RTC\src\RTC.h"
 #include <Arduino.h>
 #include <107-Arduino-UniqueId.h>
 #include <Time.h>
@@ -83,6 +84,7 @@ uint8_t sensor_address = 0x76;
 #ifndef MY_DEBUG
 #define MY_DEBUG
 #endif
+// #define MY_DEBUG  (1)
 
 #define USE_OLD_LOOP
 
@@ -175,33 +177,9 @@ std::map<std::string, DstPeriod> dst_start_end = {
 int time_now = now();
 
 int wifiStatus = WL_IDLE_STATUS;
-IPAddress timeServer(162, 159, 200, 123); // pool.ntp.org NTP server
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
 WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
 NTPClient timeClient(Udp);
-
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address) {
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
-}
 
 void initBannerText(char* buf, const char* literal) {
   memset(buf, 0, BANNER_LEN);
@@ -259,43 +237,6 @@ void connectToWiFi() {
 
   Serial.println("Connected to WiFi");
   printWifiStatus();
-}
-
-/**
- * Calculates the current unix time, that is the time in seconds since Jan 1 1970.
- * It will try to get the time from the NTP server up to `maxTries` times,
- * then convert it to Unix time and return it.
- * You can optionally specify a time zone offset in hours that can be positive or negative.
-*/
-unsigned long getUnixTime(int8_t timeZoneOffsetHours = 0, uint8_t maxTries = 5) {
-  // Try up to `maxTries` times to get a timestamp from the NTP server, then give up.
-  for (size_t i = 0; i < maxTries; i++) {
-    sendNTPpacket(timeServer); // send an NTP packet to a time server
-    // wait to see if a reply is available
-    delay(1000);
-
-    if (Udp.parsePacket()) {
-      Serial.println("packet received");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-      //the timestamp starts at byte 40 of the received packet and is four bytes,
-      //or two words, long. First, extract the two words:
-      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-      
-      // Combine the four bytes (two words) into a long integer
-      // this is NTP time (seconds since Jan 1 1900):
-      unsigned long secsSince1900 = highWord << 16 | lowWord;
-
-      // Now convert NTP time into everyday time:
-      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-      const unsigned long seventyYears = 2208988800UL;
-      unsigned long secondsSince1970 = secsSince1900 - seventyYears + (timeZoneOffsetHours * 3600);
-      return secondsSince1970;
-    }
-  }
-
-  return 0;
 }
 
 void unblockI2CBus(uint8_t sclPin) {
